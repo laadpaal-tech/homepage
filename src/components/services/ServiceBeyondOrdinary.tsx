550a371b68f7a0967ba2cb0370a9f4e19f137ba8
Marcin Czenko <marcin.czenko@pm.me>
2024-06-07 06:38:15 +0200

    one more position fix

--- src/components/services/ServiceBeyondOrdinary.tsx
+++ src/components/services/ServiceBeyondOrdinary.tsx
@@ -1,7 +1,7 @@
 const ServiceBeyondOrdinary = () => (
   <div className='flex h-full w-full items-start justify-center'>
     <div className='relative flex w-full flex-col gap-6 bg-theme-blue p-6 @3xl:h-[500px] @3xl:flex-row @5xl:h-[450px] '>
-      <div className='absolute bottom-0 left-0 right-0 top-0 @3xl:hidden'>
+      <div className='absolute left-0 right-0 top-0 @3xl:hidden'>
         <img className='w-full' src='/img/workingdesk.jpg' />
         <div className='absolute bottom-0 left-0 right-0 h-[120px] bg-gradient-to-t from-theme-blue to-[rgba(66,97,128,0.7)] @md:h-[200px] @lg:h-[240px] @xl:h-[220px] @2xl:h-[300px]'></div>
       </div>
@@ -10,7 +10,7 @@ const ServiceBeyondOrdinary = () => (
           className='max-h-[450px] w-full object-cover'
           src='/img/workingdesk.jpg'
         />
-        <div className='absolute bottom-0 left-0 right-0 top-0 bg-black opacity-70' />
+        <div className='absolute bottom-0 left-0 right-0 top-0 bg-black opacity-70'></div>
       </div>
       <section className='z-10 mt-[120px] text-lg font-extralight text-white @lg:px-10 @xl:mt-[200px] @3xl:mt-0 @3xl:text-xl'>
         <h2 className='mb-4 text-3xl font-bold @5xl:text-4xl'>Buiten gewoon</h2>
